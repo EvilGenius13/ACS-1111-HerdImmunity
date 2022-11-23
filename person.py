1533de439c2929dfa16e5f759c9d9e892f427e72
Josh Faigan <djjoker13@gmail.com>
2022-11-23 17:27:30 -0500

    MOAR TESTS

--- person.py
+++ person.py
@@ -31,76 +31,78 @@ class Person(object):
 
 
 
-testperson = Person(1, None)
-print(testperson.infection, testperson.is_vaccinated)
-
-    # This section is incomplete finish it and use it to test your Person class
-    # TODO Define a vaccinated person and check their attributes
-def vac_test():
+    
+    # Vaccinated Test
+def vaccinated_person_test():
     vaccinated_person = Person(1, True)
     assert vaccinated_person._id == 1
     assert vaccinated_person.is_alive is True
     assert vaccinated_person.is_vaccinated is True
     assert vaccinated_person.infection is None
 
-    # Create an unvaccinated person and test their attributes
-def unvac_test():
+    # Unvaccinated Test
+def unvaccinated_person_test():
     unvaccinated_person = Person(2, False)
     assert unvaccinated_person._id == 2
     assert unvaccinated_person.is_alive is True
     assert unvaccinated_person.is_vaccinated is False
     assert unvaccinated_person.infection is None
+   
+    # Infected Test
+def infected_person_test():
+    virus = Virus("Dysentery", 0.7, 0.2)
+    infected_person = Person(3, False, virus)
+    assert infected_person._id == 3
+    assert infected_person.is_alive is True
+    assert infected_person.is_vaccinated is False
+    assert infected_person.infection is virus
+    
+def survived_infection_test():
+    pass
 
 if __name__ == "__main__":
-    vac_test()
-    unvac_test()
+    print('is this working?')
+    vaccinated_person_test()
+    unvaccinated_person_test()
 
-    # # Test an infected person. An infected person has an infection/virus
-    # # Create a Virus object to give a Person object an infection
-    # virus = Virus("Dysentery", 0.7, 0.2)
-    # # Create a Person object and give them the virus infection
-    # infected_person = Person(3, False, virus)
-    # # TODO: complete your own assert statements that test
-    # # the values of each attribute
-    # # assert ...
+    people = []
+    for i in range(1, 100):
+        # TODO Make a person with an infection
+        # TODO Append the person to the people list
+        pass
 
-    # # You need to check the survival of an infected person. Since the chance
-    # # of survival is random you need to check a group of people. 
-    # # Create a list to hold 100 people. Use the loop below to make 100 people
-    # people = []
-    # for i in range(1, 100):
-    #     # TODO Make a person with an infection
-    #     # TODO Append the person to the people list
-    #     pass
 
-    # # Now that you have a list of 100 people. Resolve whether the Person 
-    # # survives the infection or not by looping over the people list. 
+    # You need to check the survival of an infected person. Since the chance
+    # of survival is random you need to check a group of people. 
+    # Create a list to hold 100 people. Use the loop below to make 100 people
+    # Now that you have a list of 100 people. Resolve whether the Person 
+    # survives the infection or not by looping over the people list. 
 
-    # # for person in people:
-    # #     # For each person call that person's did_survive_infection method
-    # #     survived = person.did_survive_infection()
+    # for person in people:
+    #     # For each person call that person's did_survive_infection method
+    #     survived = person.did_survive_infection()
 
-    # # Count the people that survived and did not survive: 
+    # Count the people that survived and did not survive: 
    
-    # # did_survived = 0
-    # # did_not_survive = 0
+    # did_survived = 0
+    # did_not_survive = 0
 
-    # # TODO Loop over all of the people 
-    # # TODO If a person is_alive True add one to did_survive
-    # # TODO If a person is_alive False add one to did_not_survive
+    # TODO Loop over all of the people 
+    # TODO If a person is_alive True add one to did_survive
+    # TODO If a person is_alive False add one to did_not_survive
 
-    # # TODO When the loop is complete print your results.
-    # # The results should roughly match the mortality rate of the virus
-    # # For example if the mortality rate is 0.2 rough 20% of the people 
-    # # should succumb. 
+    # TODO When the loop is complete print your results.
+    # The results should roughly match the mortality rate of the virus
+    # For example if the mortality rate is 0.2 rough 20% of the people 
+    # should succumb. 
 
-    # # Stretch challenge! 
-    # # Check the infection rate of the virus by making a group of 
-    # # unifected people. Loop over all of your people. 
-    # # Generate a random number. If that number is less than the 
-    # # infection rate of the virus that person is now infected. 
-    # # Assign the virus to that person's infection attribute. 
+    # Stretch challenge! 
+    # Check the infection rate of the virus by making a group of 
+    # unifected people. Loop over all of your people. 
+    # Generate a random number. If that number is less than the 
+    # infection rate of the virus that person is now infected. 
+    # Assign the virus to that person's infection attribute. 
 
-    # # Now count the infected and uninfect people from this group of people. 
-    # # The number of infectedf people should be roughly the same as the 
-    # # infection rate of the virus.
+    # Now count the infected and uninfect people from this group of people. 
+    # The number of infectedf people should be roughly the same as the 
+    # infection rate of the virus.
