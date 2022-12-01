f549d6e337b0b06aab7945eefdc37cb89f7e461a
Josh Faigan <djjoker13@gmail.com>
2022-11-30 21:15:49 -0500

    quick commit

--- simulation.py
+++ simulation.py
@@ -4,6 +4,7 @@ from logger import Logger
 from virus import Virus
 #TODO: Maybe try and make a logger to a readme.md file?
 
+# * OKAY
 class Simulation(object):
     def __init__(self, virus, pop_size, vacc_percentage, initial_infected=1):
         # TODO: Create a Logger object and bind it to self.logger.
@@ -28,6 +29,7 @@ class Simulation(object):
         self.logger = Logger(self.file_name)
         self.population = self._create_population()
 
+    # * OKAY
     def _create_population(self):
         start_population = []
         #Vaccinated
@@ -65,7 +67,29 @@ class Simulation(object):
         self.logger.write_metadata(self.pop_size, self.vacc_percentage, virus.name, virus.mortality_rate, virus.repro_rate)
         return start_population
 
+    # * OKAY
     def _simulation_should_continue(self):
+        sim_check_dead = 0
+        sim_check_vaccinated = 0
+        sim_check_alive = 0
+        for person in self.population:
+            if person.is_alive == False:
+                sim_check_dead += 1
+            if person.is_vaccinated == True:
+                sim_check_vaccinated += 1
+            if person.is_alive == True:
+                sim_check_alive += 1
+       
+        # * Testing
+        print(sim_check_dead)
+        print(sim_check_vaccinated)
+        print(sim_check_alive)
+        
+        if sim_check_dead == self.pop_size or sim_check_vaccinated == self.pop_size:
+            return False
+        else: 
+            return True
+        
         # This method will return a booleanb indicating if the simulation 
         # should continue. 
         # The simulation should not continue if all of the people are dead, 
@@ -74,21 +98,27 @@ class Simulation(object):
         # if the simulation should continue or False if not.
         pass
 
+    # ! STILL NEEDS TO BE DONE
     def run(self):
         # This method starts the simulation. It should track the number of 
         # steps the simulation has run and check if the simulation should 
         # continue at the end of each step. 
-
-        time_step_counter = 0
+        self.time_step_counter = 0
         should_continue = True
+
 
         while should_continue:
             # TODO: Increment the time_step_counter
             # TODO: for every iteration of this loop, call self.time_step() 
             # Call the _simulation_should_continue method to determine if 
             # the simulation should continue
+            
+            self.time_step()
             should_continue = self._simulation_should_continue()
-            pass
+            self.time_step_counter += 1
+        print(f"Simulation complete after {self.time_step_counter} steps.")
+        #TODO: LOGGER FOR FINAL STATUS 
+
 
         # TODO: Write meta data to the logger. This should be starting 
         # statistics for the simulation. It should include the initial
@@ -97,6 +127,7 @@ class Simulation(object):
         # TODO: When the simulation completes you should conclude this with 
         # the logger. Send the final data to the logger. 
 
+    # ! STILL NEEDS TO BE DONE
     def time_step(self):
         # This method will simulate interactions between people, calulate 
         # new infections, and determine if vaccinations and fatalities from infections
@@ -109,8 +140,24 @@ class Simulation(object):
         # takes the infected person and a random person
         pass
 
+    # ! STILL NEEDS TO BE DONE
+    # ? THIS MAY NEED TO BE REWRITTEN
+    def person_randomizer(self):
+        random_person = random.choice(self.population)
+        while random_person.is_alive == False:
+            random_person = random.chouce(self.population)
+        return random_person
+
     def interaction(self, infected_person, random_person):
         # TODO: Finish this method.
+        
+        if random_person.is_alive == True:
+            if random_person.is_vaccinated == False and random_person.infection == None:
+                 chance_of_infection = random.random()
+                 if chance_of_infection < virus.repro_rate:
+                    self.newly_infected.append(random_person._id)
+                    self.total_infected += 1
+
         # The possible cases you'll need to cover are listed below:
             # random_person is vaccinated:
             #     nothing happens to random person.
@@ -122,9 +169,16 @@ class Simulation(object):
             #     Simulation object's newly_infected array, so that their infected
             #     attribute can be changed to True at the end of the time step.
         # TODO: Call logger method during this method.
-        pass
 
+    # ! STILL NEEDS TO BE DONE
     def _infect_newly_infected(self):
+        for id in self.newly_infected:
+            for person in self.population:
+                if person._id == id:
+                    person.infection = self.virus
+        self.newly_infected = []
+        
+        
         # TODO: Call this method at the end of every time step and infect each Person.
         # TODO: Once you have iterated through the entire list of self.newly_infected, remember
         # to reset self.newly_infected back to an empty list.
@@ -147,4 +201,3 @@ if __name__ == "__main__":
     sim = Simulation(virus, pop_size, vacc_percentage, initial_infected)
 
     sim.run()
-    sim.logger.write_metadata(pop_size, vacc_percentage, virus.name, virus.mortality_rate, virus.repro_rate)
